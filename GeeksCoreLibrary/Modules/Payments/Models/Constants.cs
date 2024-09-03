4e81bba37f1e9844eaf257a3e92e49eded8d1406
Jason van der Lee <jason@in-vista.nl>
2024-09-03 13:29:39 +0200

    Revert "Removed a lot of old models that aren't used and shouldn't even be in the GCL."
    This reverts commit 3025ec260ef6b66f8e587deca854d149d81e0a0e.

--- GeeksCoreLibrary/Modules/Payments/Models/Constants.cs
+++ GeeksCoreLibrary/Modules/Payments/Models/Constants.cs
@@ -1,4 +1,10 @@
-﻿namespace GeeksCoreLibrary.Modules.Payments.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeeksCoreLibrary.Modules.Payments.Models
 {
     public static class Constants
     {
