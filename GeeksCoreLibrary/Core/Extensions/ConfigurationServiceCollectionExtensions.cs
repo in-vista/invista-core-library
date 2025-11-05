9469a84b9f0e4c51ca6e6f94442b96d424f95ea2
Jason van der Lee <jason@in-vista.nl>
2025-11-05 11:59:09 +0100

    Removed enforced allow all origins, methods, and headers through CORS in development environments

--- GeeksCoreLibrary/Core/Extensions/ConfigurationServiceCollectionExtensions.cs
+++ GeeksCoreLibrary/Core/Extensions/ConfigurationServiceCollectionExtensions.cs
@@ -82,13 +82,6 @@ namespace GeeksCoreLibrary.Core.Extensions
             if (env.IsDevelopment())
             {
                 builder.UseDeveloperExceptionPage();
-                
-                builder.UseCors(policy =>
-                {
-                    policy.AllowAnyOrigin()
-                        .AllowAnyHeader()
-                        .AllowAnyMethod();
-                });
             }
             else
             {
