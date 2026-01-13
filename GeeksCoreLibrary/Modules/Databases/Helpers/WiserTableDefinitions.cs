4efdc0a2e134f86deba7b9793090d9c2dc99921f
Jason van der Lee <jason@in-vista.nl>
2026-01-13 22:19:21 +0100

    Added group_icon column to wiser_module table

--- GeeksCoreLibrary/Modules/Databases/Helpers/WiserTableDefinitions.cs
+++ GeeksCoreLibrary/Modules/Databases/Helpers/WiserTableDefinitions.cs
@@ -1087,7 +1087,7 @@ public class WiserTableDefinitions
         new WiserTableDefinitionModel
         {
             Name = WiserTableNames.WiserModule,
-            LastUpdate = new DateTime(2024, 11, 06),
+            LastUpdate = new DateTime(2026, 1, 13),
             Columns = new List<ColumnSettingsModel>
             {
                 new("id", MySqlDbType.UInt64, notNull: true, isPrimaryKey: true, autoIncrement: true),
@@ -1099,6 +1099,7 @@ public class WiserTableDefinitions
                 new("color", MySqlDbType.VarChar, 8),
                 new("type", MySqlDbType.VarChar, 255),
                 new("group", MySqlDbType.VarChar, 100),
+                new("group_icon", MySqlDbType.VarChar, 255),
                 new("custom_script", MySqlDbType.MediumText),
                 new("is_fullscreen", MySqlDbType.Int16, 4, notNull: true, defaultValue: "0")
             }
