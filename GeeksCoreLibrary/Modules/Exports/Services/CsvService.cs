ea076e137139b8307d39471efa50809877d04a80
Ferry van de Graaf <ferryvandegraaf@gmail.com>
2026-01-23 08:43:54 +0100

    Added functionality to Export Datatable to CSV

--- GeeksCoreLibrary/Modules/Exports/Services/CsvService.cs
+++ GeeksCoreLibrary/Modules/Exports/Services/CsvService.cs
@@ -8,6 +8,8 @@ using GeeksCoreLibrary.Core.DependencyInjection.Interfaces;
 using GeeksCoreLibrary.Core.Helpers;
 using GeeksCoreLibrary.Modules.Exports.Interfaces;
 using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Text;
 
 namespace GeeksCoreLibrary.Modules.Exports.Services;
 
@@ -48,4 +50,43 @@ public class CsvService : ICsvService, IScopedService
 
         return stringWriter.ToString();
     }
+    
+    public static byte[] DataTableToCsvBytes(DataTable table, string delimiter = ";")
+    {
+        using var memoryStream = new MemoryStream();
+
+        // Excel-friendly UTF-8 (with BOM)
+        using var writer = new StreamWriter(
+            memoryStream,
+            new UTF8Encoding(encoderShouldEmitUTF8Identifier: true),
+            leaveOpen: true
+        );
+
+        using var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = true,
+            Delimiter = delimiter
+        });
+
+        // Header
+        foreach (DataColumn column in table.Columns)
+        {
+            csv.WriteField(column.ColumnName);
+        }
+        csv.NextRecord();
+
+        // Rows
+        foreach (DataRow row in table.Rows)
+        {
+            foreach (var field in row.ItemArray)
+            {
+                csv.WriteField(field);
+            }
+            csv.NextRecord();
+        }
+
+        writer.Flush();
+        return memoryStream.ToArray();
+    }
+    
 }
